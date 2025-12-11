81bd47ebfb9278a488550875309b347c3d982d07
Felix-CtrlV <kaungswan59@gmail.com>
2025-12-11 14:48:32 +0630

    directory setup

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import NewFile from './pages/newpage.jsx';
+
+function App() {
+  return <NewFile />;
+}
+
+export default App;
---
+++ FrontEnd/src/pages/newpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import ThreeScene from '../components/ThreeScene';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold mb-6">Welcome to Virtual Mall</h1>
+      {/* <ThreeScene /> */}
+    </div>
+  );
+}
