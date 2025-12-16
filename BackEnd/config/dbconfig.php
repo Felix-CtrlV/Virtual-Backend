8ead50df47aaf388e56a59f471b73b02b28ad834
Felix-CtrlV <kaungswan59@gmail.com>
2025-12-17 00:39:22 +0630

    test

--- BackEnd/config/dbconfig.php
+++ BackEnd/config/dbconfig.php
@@ -1,7 +1,7 @@
 <?php 
 $host = "mysql-16bab7fd-kaungswan59-86a3.h.aivencloud.com";
 $username = "avnadmin";
-// $password = "AVNS_kEHRdCr_cgK14vicoWI";
+$password = "AVNS_kEHRdCr_cgK14vicoWI";
 $port = 12525;
 $database = "Malltiverse";
 
