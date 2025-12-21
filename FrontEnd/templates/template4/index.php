9a3dde44758ef2b2258786d9de5a7324303224ce
Zenji <aungkaungmyat.20mtla@gmail.com>
2025-12-21 13:01:49 +0630

    start

--- FrontEnd/templates/template4/index.php
+++ FrontEnd/templates/template4/index.php
@@ -3,22 +3,44 @@ if (!isset($conn)) {
     include '../../../BackEnd/config/dbconfig.php';
 }
 
-$supplier_id = (int)$supplier['supplier_id'];
+if (!isset($_GET['supplier_id'])) {
+    die('Supplier ID missing');
+}
 
-$assets_stmt = mysqli_prepare($conn, "SELECT * FROM shop_assets WHERE supplier_id = ?");
+$supplier_id = (int) $_GET['supplier_id'];
+
+$supplier_stmt = mysqli_prepare(
+    $conn,
+    "SELECT * FROM suppliers WHERE supplier_id = ? LIMIT 1"
+);
+
+mysqli_stmt_bind_param($supplier_stmt, "i", $supplier_id);
+mysqli_stmt_execute($supplier_stmt);
+$supplier_result = mysqli_stmt_get_result($supplier_stmt);
+
+if (!$supplier_result || mysqli_num_rows($supplier_result) === 0) {
+    die('Supplier not found');
+}
+
+$supplier = mysqli_fetch_assoc($supplier_result);
+mysqli_stmt_close($supplier_stmt);
+
+$assets_result = false;
+
+$assets_stmt = mysqli_prepare(
+    $conn,
+    "SELECT * FROM shop_assets WHERE supplier_id = ? LIMIT 1"
+);
+
 if ($assets_stmt) {
     mysqli_stmt_bind_param($assets_stmt, "i", $supplier_id);
     mysqli_stmt_execute($assets_stmt);
     $assets_result = mysqli_stmt_get_result($assets_stmt);
-} else {
-    $assets_result = false;
+    mysqli_stmt_close($assets_stmt);
 }
 
 if ($assets_result && mysqli_num_rows($assets_result) > 0) {
     $shop_assets = mysqli_fetch_assoc($assets_result);
-    if (isset($assets_stmt)) {
-        mysqli_stmt_close($assets_stmt);
-    }
 } else {
     $shop_assets = [
         'logo' => 'default_logo.png',
@@ -27,6 +49,7 @@ if ($assets_result && mysqli_num_rows($assets_result) > 0) {
         'secondary_color' => '#2c3e50'
     ];
 }
+
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 $allowed_pages = ['home', 'about', 'products', 'contact'];
