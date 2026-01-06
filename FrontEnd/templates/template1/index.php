5a1358e9ab3aa975495da97d3fddb3d334857d35
Kanyaw Paw <naha72478@gmail.com>
2026-01-06 09:51:44 +0630

    hhh

--- FrontEnd/templates/template1/index.php
+++ FrontEnd/templates/template1/index.php
@@ -1,36 +1,4 @@
 <?php
-<<<<<<< HEAD
-include("partial/header.php");
-?>
-
-<div class="content">
-    <img class="homeimg" src="" alt="">
-    <div class="cardslot">
-        <div class="card">
-            <img class="cardimg" src="" alt="">
-            <h1 class="cardtitle"></h1>
-            <a href="categoryView.php?category=chinese" class="">View</a>
-        </div>
-        <div class="card">
-            <img class="cardimg" src="" alt="">
-            <h1 class="cardtitle"></h1>
-            <a href="categoryView.php?category=thai" class="">View</a>
-        </div>
-        <div class="card">
-            <img class="cardimg" src="" alt="">
-            <h1 class="cardtitle"></h1>
-            <a href="categoryView.php?category=myanmar" class="">View</a>
-        </div>
-    </div>
-
-</div>
-
-<?php
-include("partial/footer.php");
-?>
-
-<!-- run the file that is inside the include parameter as soon as it's launch -->
-=======
 if (!isset($conn)) {
     include '../../../BackEnd/config/dbconfig.php';
 }
@@ -105,4 +73,3 @@ $page_path = __DIR__ . "/pages/$page.php";
 </body>
 </html>
 
->>>>>>> 31de85602fffdab61d7b1f644f4b36b2e1e652dd
