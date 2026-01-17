28ea872770d200d89731744ec9919775dbe75ee4
HannaMeiko <kayzinh4@gmail.com>
2026-01-17 14:27:01 +0630

    hanna

--- FrontEnd/utils/Ordered.php
+++ FrontEnd/utils/Ordered.php
@@ -1,7 +1,7 @@
 <?php
 
 function placeOrder($conn, $customer_id, $supplier_id) {
-  
+    
     $cart_query = "SELECT c.variant_id, c.quantity, p.price 
                    FROM cart c 
                    JOIN product_variant v ON c.variant_id = v.variant_id 
@@ -23,26 +23,39 @@ function placeOrder($conn, $customer_id, $supplier_id) {
 
     if (empty($cart_items)) return false;
 
-    $order_code = "ORD-" . strtoupper(substr(md5(time()), 0, 8));
-    $payment_method = "crediverse";
-    $order_status = "Confirm";
-    $order_date = date('Y-m-d H:i:s');
+    mysqli_begin_transaction($conn);
 
-    $order_sql = "INSERT INTO orders (order_code, supplier_id, customer_id, price, payment_method, order_status, order_date) 
-                  VALUES (?, ?, ?, ?, ?, ?, ?)";
-    
-    $o_stmt = mysqli_prepare($conn, $order_sql);
-    mysqli_stmt_bind_param($o_stmt, "siidsss", $order_code, $supplier_id, $customer_id, $grand_total, $payment_method, $order_status, $order_date);
-    
-    if (mysqli_stmt_execute($o_stmt)) {
+    try {
+        $order_code = "ORD-" . strtoupper(substr(md5(time()), 0, 8));
+        $payment_method = "crediverse";
+        $order_status = "confirm";
+        $order_date = date('Y-m-d H:i:s');
+
+        $order_sql = "INSERT INTO orders (order_code, supplier_id, customer_id, price, payment_method, order_status, order_date) 
+                      VALUES (?, ?, ?, ?, ?, ?, ?)";
+        
+        $o_stmt = mysqli_prepare($conn, $order_sql);
+        mysqli_stmt_bind_param($o_stmt, "siidsss", $order_code, $supplier_id, $customer_id, $grand_total, $payment_method, $order_status, $order_date);
+        
+        if (!mysqli_stmt_execute($o_stmt)) {
+            throw new Exception("Order insertion failed");
+        }
+
         $order_id = mysqli_insert_id($conn);
 
         $detail_sql = "INSERT INTO order_detail (order_id, variant_id, quantity) VALUES (?, ?, ?)";
         $d_stmt = mysqli_prepare($conn, $detail_sql);
 
+        $update_stock_sql = "UPDATE product_variant SET quantity = quantity - ? WHERE variant_id = ?";
+        $u_stmt = mysqli_prepare($conn, $update_stock_sql);
+
         foreach ($cart_items as $item) {
+ 
             mysqli_stmt_bind_param($d_stmt, "iii", $order_id, $item['variant_id'], $item['quantity']);
             mysqli_stmt_execute($d_stmt);
+
+            mysqli_stmt_bind_param($u_stmt, "ii", $item['quantity'], $item['variant_id']);
+            mysqli_stmt_execute($u_stmt);
         }
 
         $del_sql = "DELETE FROM cart WHERE customer_id = ? AND supplier_id = ?";
@@ -50,8 +63,14 @@ function placeOrder($conn, $customer_id, $supplier_id) {
         mysqli_stmt_bind_param($del_stmt, "ii", $customer_id, $supplier_id);
         mysqli_stmt_execute($del_stmt);
 
+        mysqli_commit($conn);
         return true;
+
+    } catch (Exception $e) {
+ 
+        mysqli_rollback($conn);
+        return false;
     }
-    return false;
+    
 }
 ?>
