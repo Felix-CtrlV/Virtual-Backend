f9a92a8a3cb68852f2da43f5bc2fbd828b887039
Zenji <aungkaungmyat.20mtla@gmail.com>
2026-01-26 21:15:18 +0630

    push

--- FrontEnd/templates/template4/pages/home.php
+++ FrontEnd/templates/template4/pages/home.php
@@ -273,8 +273,8 @@ $tagline = $supplier['tagline'] ?? 'Redefine Your Limits.';
 
 <section class="hero-section">
     <?php if ($shop_assets['template_type'] == 'video'): ?>
-        <video class="hero-media" autoplay muted loop playsinline src="../uploads/shops/<?= $supplier_id?>/<?= $shop_assets['banner'] ?>"></video>
-    
+        <video class="hero-media" autoplay muted loop playsinline src="../uploads/shops/<?= $supplier_id ?>/<?= $shop_assets['banner'] ?>"></video>
+
     <?php else: ?>
         <img src="../uploads/shops/<?= $supplier_id ?>/<?= $banner1 ?>" alt="Hero Banner" class="hero-media" style="transform: scale(1.1);">
     <?php endif; ?>
